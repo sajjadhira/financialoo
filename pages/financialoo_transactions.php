5e5b62c042e66c04f5ddb09eb29653c39d1f5fdb
sajjadhira <sajjad.hira12@gmail.com>
2023-09-10 00:15:24 +0600

    transaction modeule updated

--- pages/financialoo_transactions.php
+++ pages/financialoo_transactions.php
@@ -1,19 +1,40 @@
 <?php
 function financialoo_transactions()
 {
-    $title = 'Transactions';
+  $title = 'Transactions';
+
+  if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+    $action = sanitize_text_field($action);
+  } else {
+    $action = '';
+  }
 ?>
-<div class="wrap">
-  <div class="bg-gray-100 items-center pt-8">
-    <?php
-    include FINANCIALOO_PLUGIN_DIR . '/pages/header.php';
-    ?>
+  <div class="wrap">
+    <div class="bg-gray-100 items-center pt-8">
+      <?php
+      include FINANCIALOO_PLUGIN_DIR . '/pages/header.php';
+      ?>
+      <div class="bg-white p-8 rounded shadow-md">
+        <h2 class="text-xl font-bold mb-4"><?php echo $title; ?></h2>
+        <div class="relative overflow-x-auto"></div>
+      </div>
+    </div>
+
     <div class="bg-white p-8 rounded shadow-md">
-      <h2 class="text-xl font-bold mb-4"><?php echo $title; ?></h2>
-      <div class="relative overflow-x-auto"></div>
+
+      <?php
+      if ($action == "delete") {
+        include FINANCIALOO_PLUGIN_DIR . '/pages/partials/transactions/delete.php';
+      } else {
+        include FINANCIALOO_PLUGIN_DIR . '/pages/partials/transactions/list.php';
+      }
+      ?>
+
+
+
     </div>
   </div>
-</div>
 
 <?php
 }
